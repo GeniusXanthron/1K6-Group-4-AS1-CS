2052b538d486c96f736c308957081f133a16bd52
TheAmityD <88772700+TheAmityD@users.noreply.github.com>
2022-05-09 21:46:55 +0800

    Initial website

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="home.css">
+    <link rel="stylesheet" href="https://use.typekit.net/phx0aia.css">
+    <title>KGPN Directories</title>
+</head>
+<body>
+    <div class="topbar">
+        <h1>Kolej GENIUS@Pintar Negara</h1>
+        <h2>Directory</h2>
+    </div>
+    <div class="mainpanel">
+        <form action="keyin">
+            <p>Name: </p><input type="text" name="name">
+            <p>Phone Number: </p><input type="text" name="phno">
+            <p>Department: </p><input type="text" name="dept">
+            <br><br>
+            <input type="submit" value="Enter">
+        </form>
+    </div>
+</body>
+</html>
