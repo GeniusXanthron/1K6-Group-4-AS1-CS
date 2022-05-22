961ef96abf2936d48408c1c9b745ea273db31c4f
TheAmityD <88772700+TheAmityD@users.noreply.github.com>
2022-05-22 12:22:54 +0800

    Added a lotta things
    Co-Authored-By: Coral-Reefs <89334712+Coral-Reefs@users.noreply.github.com>

--- insert.php
+++ insert.php
@@ -9,7 +9,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="main.css">
-    <link rel="stylesheet" href="home.css">
+    <link rel="stylesheet" href="insert.css">
     <link rel="stylesheet" href="https://use.typekit.net/phx0aia.css">
     <title>KGPN Directories</title>
 </head>
@@ -17,21 +17,25 @@
     <div class="topbar">
         <img src="assets/genius.jpg" alt="">
         <h1>Kolej GENIUS@Pintar Negara</h1>
-        <h2>Directory: Search</h2>
+        <h2>Directory: Insert</h2>
     </div>
     <div class="mainpanel">
-        <form method="post" action="index.php">
+        <form action="insert.php">
             <div class="formy">
-                <div id="form"><p>Search: </p><input type="text" name="search"></div>
+                <div id="formname"><p>Name: </p><input type="text" name="name"></div>
+                <div id="formphno"><p>Phone Number: </p><input type="text" name="phno"></div>
+                <div id="formdept"><p>Department: </p><input type="text" name="dept"></div>
             </div>
             <br><br>
             <input type="submit" name="submeet" value="Enter">
         </form>
         <?php
             if(isset($_POST["submeet"])) {
-                $search_query = $_POST['search'];
+                $name = $_POST['name'];
+                $phone = $_POST['phno'];
+                $dept = $_POST['dept'];
 
-                $query = "select * from staff where name like '$search_query'";
+                $query = "insert into staff values ('','$name','$phone','$dept')";
                 $query_run = mysqli_query($con,$query);
             }
         ?>
